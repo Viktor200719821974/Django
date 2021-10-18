962ac0df60a0cc58b84913e3095382cad410cc15
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-18 14:39:36 +0200

    master add path(hello)

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myfirstapp.views import hello
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path()
+    path('hello', hello)
 ]
