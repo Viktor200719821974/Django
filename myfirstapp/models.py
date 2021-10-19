3bb2b9115350fba75dab5a160faf8288e57439dd
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-19 16:08:33 +0200

    django 2 break

--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class CarModel(models.Model):
-    class Meta:
-        db_table = 'cars'
-    brand = models.CharField(max_length=20)
-    model = models.CharField(max_length=20)
-    year = models.IntegerField()
