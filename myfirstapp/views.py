cb097bea01cba23e7ac18fb006c34f2d889fd1c6
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-19 22:20:02 +0200

    django 1  read

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 def calkulator(request, a, b, c):
-    print(a,b,c)
-    # if c == '+':
-    #   x = a + b
-    # elif c == '-':
-    #   x = a - b
-    # elif c == '*':
-    #  x = a * b
-    # elif c == '/':
-    #  x = a / b
+    print(a, b, c)
+    if c == '+':
+        x = a + b
+    elif c == '-':
+        x = a - b
+    elif c == '*':
+        x = a * b
+    elif c == ':':
+        x = a / b
 
-    return render(request, 'calkulator.html',{'a':a,'b':b,'c':c})
+    return render(request, 'calkulator.html', {'a': a, 'b': b, 'c': c, 'x':x})
 
-# def users(request,name):
-#     print(name)
-#     return render(request, 'calkulator.html', {'user':name})
